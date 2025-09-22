c80f27e627548f0293797947af1b89adc8b5837e
Polo <148433691+MugishaProsper@users.noreply.github.com>
2025-09-22 18:51:15 +0200

    resolved cors origin

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use('/api/webhooks', webhookRoutes)
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:3000',
+  origin: process.env.CLIENT_URL || 'http://localhost:3000' || "https://carhub-rw.vercel.app",
   credentials: true
 }))
 
