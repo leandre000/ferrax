07d2907889a22fd2a9107b4fd27fcfa784aec09b
Polo <148433691+MugishaProsper@users.noreply.github.com>
2025-10-07 14:55:07 +0200

    refactored code

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -28,12 +28,6 @@ export const register = async (req, res) => {
             phone: phone.trim(),
             password: hashedPassword
         });
-        // Always use a 6-digit, zero-padded string OTP
-        // const verificationCode = String(Math.floor(Math.random() * 1000000)).padStart(6, '0')
-        // const message = `Your OTP is ${verificationCode}`
-        // await sendPhoneVerificationCode(user.phone, message)
-        // user.otpCode = verificationCode
-        // user.otpExpiresAt = Date.now() + 60 * 60 * 1000 // 1 hour
         await user.save();
         // Do NOT issue JWT yet; wait until OTP verification completes
         res.status(201).json({ message: 'User registered successfully', success: true })
